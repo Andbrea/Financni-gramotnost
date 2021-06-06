ba100a71f79c42323f98804df55af496e1d04cb2
ivetvrobel <iveta.vrobelova@gmail.com>
2021-06-06 12:51:50 +0200

    minor changes

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -4,45 +4,50 @@ import { Link } from 'react-router-dom';
 import Karta from './Karta';
 
 const Home = () => {
-  return (
-    <>
-      <div className="uvod-box">
-        <div className="uvod-hlavicka">
-          <img src="assets/img/lista.svg" />
-          <div className="uvod-header"> </div>
-          <div className="uvod-nadpis">FINANČNÍ GRAMOTNOST</div>
-          <div className="uvod-podnadpis">S penězi si poradím!</div>
-        </div>
-        <div className="uvod-text">
-          Vítejte na naši informační stránce, která Vás seznámí se základními
-          pojmy finanční problematiky. Pro další informace,klikněte na tlačítko
-          pod obrázkem dle vlastního výběru - děti, teenageři, dospělí.{' '}
-        </div>
-        <div className="uvod-skupiny">
-          <div className="uvod-karta">
-            <Karta title="Děti" to="/deti" cesta="/assets/img/Kids.svg" />
-          </div>
-          <div className="uvod-karta">
-            <Karta
-              title="Teenager"
-              to="/teenageri"
-              cesta="/assets/img/Teens.svg"
-            />
-          </div>
-          <div className="uvod-karta">
-            <Karta
-              title="Dospělí"
-              to="/dospeli"
-              cesta="/assets/img/Adults.svg"
-            />
-          </div>
-        </div>
-        <div className="uvod-paticka">
-          2021, Andrea Bravencová, Iveta Vrobelová, Jitka Ševčíková{' '}
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="uvod-box">
+                <div className="uvod-hlavicka">
+                    <img src="assets/img/lista.svg" />
+                    <div className="uvod-header"> </div>
+                    <div className="uvod-nadpis">FINANČNÍ GRAMOTNOST</div>
+                    <div className="uvod-podnadpis">S penězi si poradím!</div>
+                </div>
+                <div className="uvod-text">
+                    Vítejte na naši informační stránce, která Vás seznámí se
+                    základními pojmy finanční problematiky. Pro další
+                    informace,klikněte na tlačítko pod obrázkem dle vlastního
+                    výběru - děti, teenageři, dospělí.{' '}
+                </div>
+                <div className="uvod-skupiny">
+                    <div className="uvod-karta">
+                        <Karta
+                            title="Děti"
+                            to="/deti"
+                            cesta="/assets/img/Kids.svg"
+                        />
+                    </div>
+                    <div className="uvod-karta">
+                        <Karta
+                            title="Teenager"
+                            to="/teenageri"
+                            cesta="/assets/img/Teens.svg"
+                        />
+                    </div>
+                    <div className="uvod-karta">
+                        <Karta
+                            title="Dospělí"
+                            to="/dospeli"
+                            cesta="/assets/img/Adults.svg"
+                        />
+                    </div>
+                </div>
+                <div className="uvod-paticka">
+                    2021, Andrea Bravencová, Iveta Vrobelová, Jitka Ševčíková{' '}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
