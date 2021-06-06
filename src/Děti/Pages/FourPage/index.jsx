b24419f05b6042efd31c1d3767e87cba7cfe1802
Andbrea <a.bravencovaa@gmail.com>
2021-06-06 13:24:07 +0200

    Seznam

--- src/Děti/Pages/FourPage/index.jsx
+++ src/Děti/Pages/FourPage/index.jsx
@@ -1,56 +1,49 @@
 import React from "react";
+import "./style.css";
 import * as ReactDOM from "react-dom";
 import Dragula from "react-dragula";
-import "./style.css";
 
 export class FourPage extends React.Component {
   constructor(props) {
     super(props);
+    this.myRef = React.createRef();
     this.state = { color: "" };
   }
+  handleSubmit = () => {
+    const vysledek = Array.from(this.myRef.children).map(
+      (item) => item.className
+    );
+  };
   render() {
+    console.log("aaa");
     return (
       <>
+        <h2>Kvíz</h2>
+        <p>Seřaď správně položky v seznamu od nejdražší po nejlevnější</p>
         <div
           onClick={function () {
             this.setState({ color: "red" });
           }}
-          className={this.color ? "container onSelected" : "container"}
+          /* className={this.color ? "container onSelected" : "container"} */
+          className={"boxes"}
           ref={this.dragulaDecorator}
         >
-          <div className="box">blaaaaaaa</div>
-          <div className="box">bleeeeeee</div>
-          <div className="box">bliiiiiii</div>
-          <div className="box">bluuuuuuu</div>
+          <div className="chleba">Chleba</div>
+          <div className="lizatko">Lízátko</div>
+          <div className="pocitac">Počítač</div>
+          <div className="auto">Auto</div>
         </div>
-        <p>Vybral jsi</p>
+        <button className="btnElm" onClick={this.handleSubmit}>
+          Vyhodnotit
+        </button>
       </>
     );
   }
   dragulaDecorator = (componentBackingInstance) => {
+    this.myRef = componentBackingInstance;
     if (componentBackingInstance) {
       let options = {};
-      Dragula([componentBackingInstance], options);
+      const Zkouska = Dragula([componentBackingInstance], options);
     }
   };
 }
-
-/* export class App extends React.Component {
-  render () {
-    return <div className='container' ref={this.dragulaDecorator}>
-      <div>Swap me around</div>
-      <div>Swap her around</div>
-      <div>Swap him around</div>
-      <div>Swap them around</div>
-      <div>Swap us around</div>
-      <div>Swap things around</div>
-      <div>Swap everything around</div>
-    </div>;
-  },
-  dragulaDecorator = (componentBackingInstance) => {
-    if (componentBackingInstance) {
-      let options = { };
-      Dragula([componentBackingInstance], options);
-    }
-  };
-}); */
