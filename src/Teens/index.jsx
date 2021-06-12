27ae9706bb22c8ce99276dac206bb8bec18bdc6b
ivetvrobel <iveta.vrobelova@gmail.com>
2021-06-12 16:04:57 +0200

    changes

--- src/Teens/index.jsx
+++ src/Teens/index.jsx
@@ -11,14 +11,16 @@ const Teens = () => {
                 <Route exact={true} path="/teenageri">
                     <Page
                         title="Potřebuji versus chci"
-                        path="../../assets/img/Adults.svg"
-                        altName="teens"
+                        path="../../assets/img/Teens-img/needs-wants.svg"
+                        altName="Potřebuji vs chci"
                         text="Je třeba si uvědomit, že peníze jsou tu k tomu, aby zajišťovaly hlavně naše základní lidské potřeby. Platíme jimi jídlo, bydlení, oblečení a léky. Teprve na druhém místě by měly být peníze použity na volnočasové aktivity a jiné věci. Před pořízením nové věci je vždy třeba si uvědomit, co je důležitější. Opravdu tu věc potřebuji nebo ji jenom chci? Nenechávám se ovlivnit okolím – přátelé, reklamy?"
                     />
                 </Route>
                 <Route path="/teenageri/zdrojpenez">
                     <Page
                         title="Zdroj peněz"
+                        path="../../assets/img/Teens-img/money-source.svg"
+                        altName="Zdroj peněz"
                         text="Pokud si chci něco koupit, musím na to mít peníze. Ty buď dostávám od rodičů jako kapesné, jako peněžní dárky od příbuzných nebo je mohu získat prací. Do 15 let je to spíše odměna za domácí práce a v pozdějším věku je tu možnost chodit na brigády. Kdo má odvahu a nápad, může se pustit do vlastního podnikání. Nikdy však nesmím zapomínat plnit nejprve školní povinnosti."
                     />
                 </Route>
@@ -41,6 +43,8 @@ const Teens = () => {
                 <Route path="/teenageri/platebnikarta">
                     <Page
                         title="Platební karta"
+                        path="../../assets/img/Teens-img/credit-card.svg"
+                        altName="Platební karta"
                         text="Ke každému bankovnímu účtu je vystavena platební karta, která nahrazuje placení bankovkami a mincemi. Je na ní vždy uvedeno jméno vlastníka, číslo karty, datum doby platnosti a trojciferný bezpečnostní kód. Při platbě kartou potřebuješ znát pin karty, což jsou čtyři libovolná čísla, která si musíš pamatovat. Nikdy neposkytuj tento pin, ani informace o kartě cizí osobě! Mohl bys přijít o své úspory."
                     />
                 </Route>
