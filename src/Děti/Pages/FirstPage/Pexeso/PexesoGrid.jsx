42aaf8eab1dd8a9a30aa263cc9d3d9dcebc0c99d
Andbrea <a.bravencovaa@gmail.com>
2021-07-19 05:37:27 +0200

    Update PexesoGrid.jsx

--- src/Děti/Pages/FirstPage/Pexeso/PexesoGrid.jsx
+++ src/Děti/Pages/FirstPage/Pexeso/PexesoGrid.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import "./style.css";
-import koruna from "../img/1_2004_1B.jpg";
-import dveKoruna from "../img/2_1999_1B.jpg";
-import petiKoruna from "../img/5_1999_1B.jpg";
-import desetiKoruna from "../img/Mince_10Kč_vzor_2003_rubová_strana.jpg";
-import dvacetiKoruna from "../img/20_2004_3B.jpg";
-import padesatiKoruna from "../img/50_1999_1B.jpg";
+
 
 export const Card = ({ image, isFlipped, isDisabled }) => {
   return (
